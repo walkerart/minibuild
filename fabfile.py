422dcfce3ce4e04b9c82c6ef02160b2182a0dece
Chris Thompson <chris.thompson@walkerart.org>
2012-02-15 16:21:51 -0600

    rearrange methods in nice order

--- fabfile.py
+++ fabfile.py
@@ -13,20 +13,12 @@ env.service_identifier = 'wac-collectionspace'
 env.tenant = 'walkerart'
 env.login_userid = 'admin@walkerart.org'
 
-project_dir = '/home/ubuntu/src/v2.0/'
+services_dir = '/home/ubuntu/src/v2.0/'
 minibuild_dir = '/home/ubuntu/src/minibuild/'
 
 rrun   = lambda string,*args,**kwargs: wrapped_run(string.format(**env), *args,**kwargs)
 llocal = lambda string,*args,**kwargs: wrapped_local(string.format(**env), *args,**kwargs)
         
-def _login(func):
-    "decorator which uses a session cookie"
-    def logged_in(*args,**kwargs):
-        _get_cookie()
-        func(*args,**kwargs)
-        llocal('rm ' + env.cookie_file)
-    return logged_in
-    
 def stop_server():
     "runs shutdown.sh -force and kills tomcat"
     with settings(warn_only=True):
@@ -47,81 +39,23 @@ def stop_server():
             rrun('kill -9 ' + pid)
     rm_pid()
 
-def get_pid():
-    "pid of java program using unique identifier"
-    pid = rrun("ps ww -C java | grep {service_identifier} | awk '{{print$1}}'",True)
-    puts(green("pid = " + pid))
-    return pid
-
-def rm_pid():
-    rrun('touch {CSPACE_JEESERVER_HOME}/bin/tomcat.pid')
-    rrun('rm {CSPACE_JEESERVER_HOME}/bin/tomcat.pid')
-
-def start_server():
+def start_server(func=rrun):
     "start collectionspace"
-    pid = get_pid()
+    pid = get_pid(func)
     if pid:
         puts(yellow('server appears to be running; run stop_server first'))
         return 
-    rrun('source ~/.bashrc && {CSPACE_JEESERVER_HOME}/bin/startup.sh -dservice.identifier={service_identifier}', pty=False)
-    pid = get_pid()
-
-def cat_log():
-    "see tail of catalina.out"
-    rrun("tail {CSPACE_JEESERVER_HOME}/logs/catalina.out", pty=False)
-
-def _build():
-    rrun('ant undeploy deploy', pty=False)
-
-def _get_cookie():
-    "login to collectionspace and GET tenant init to reload the configs"
-    env.cookie_file = "cookies.txt"
-    env.login_password = prompt("enter password for {}:".format(env.login_userid))
-    llocal("""\
-          wget -q --keep-session-cookies --save-cookies {cookie_file} \
-          --post-data "userid={login_userid}&password={login_password}" \
-          http://{host_string}:{http_port}/collectionspace/tenant/walkerart/login \
-          -O /dev/null \
-          """)
-
-    cookie = open(env.cookie_file).readlines().pop()
-    if "CSPACESESSID" in cookie:
-        print(green("logged in successfully"))
+    if func == llocal:
+        prefix = ''
+        kwargs = {}
     else:
-        print(red("not logged in! wrong password?"))
-        exit
+        prefix = 'source ~/.bashrc && '
+        kwargs = {'pty': False}
+    #run command will not actually start the server without setting pty=False, I don't know why.
+    func(prefix+'{CSPACE_JEESERVER_HOME}/bin/startup.sh -dservice.identifier={service_identifier}',
+         **kwargs)
+    pid = get_pid(func)
 
-@hosts('localhost')
-def local_tenant_init():
-    "shortcut of tenant_init for localhost"
-    tenant_init()
-
-@_login
-def tenant_init():
-    "hit tenant/{tenant}/init to reload configs"
-    llocal("""\
-          wget --keep-session-cookies --load-cookies {cookie_file} \
-          http://{host_string}:{http_port}/collectionspace/tenant/{tenant}/init \
-          -O - \
-          """)
-
-@hosts('localhost')
-def local_init():
-    "shortcut of auth_init for localhost"
-    auth_init()
-
-@_login
-def auth_init():
-    "hit authorities/initialise and authorities/vocab/initialize"
-    llocal('wget  --keep-session-cookies --load-cookies {cookie_file} \
-           http://{host_string}:{http_port}/collectionspace/tenant/{tenant}/authorities/initialise -O -')
-    llocal('wget  --keep-session-cookies --load-cookies {cookie_file} \
-           http://{host_string}:{http_port}/collectionspace/tenant/{tenant}/authorities/vocab/initialize -O - ')#!!z!!
-
-
-def _git_pull():
-    rrun('git pull origin custom')
-        
 def deploy(layer='application'):
     "pull updates from git origin and load 'em up"
     directory = "{path}/{layer}".\
@@ -133,24 +67,119 @@ def deploy(layer='application'):
         _build()
     start_server()
 
+def deploy_services():
+    stop_server()
+    with cd(services_dir):
+        _git_pull()
+        _build()
+        rrun('source ~/.bashrc && ant create_db import')
+    start_server()
+    
+def get_pid(func=rrun):
+    "pid of java program using unique identifier"
+    pid = func("ps ww -C java | grep {service_identifier} | awk '{{print$1}}'",True)
+    puts(green("pid = " + pid))
+    return pid
+
+def rm_pid():
+    rrun('touch {CSPACE_JEESERVER_HOME}/bin/tomcat.pid')
+    rrun('rm {CSPACE_JEESERVER_HOME}/bin/tomcat.pid')
+
+def cat_log():
+    "see tail of catalina.out"
+    rrun("tail {CSPACE_JEESERVER_HOME}/logs/catalina.out", pty=False)
+
+def _build():
+    rrun('source ~/.bashrc && ant undeploy deploy', pty=False)
+
+def _git_pull():
+    rrun('git pull origin custom')
+        
+
+##################
+# local utils
+###################
+
+def _login(func):
+    "decorator which uses a session cookie"
+    def logged_in(*args,**kwargs):
+        _get_cookie()
+        func(*args,**kwargs)
+        llocal('rm ' + env.cookie_file)
+    return logged_in
+
+@hosts('localhost')
+def local_tenant_init():
+    "shortcut of tenant_init for localhost"
+    tenant_init()
+
+@hosts('localhost')
+def local_auth_init():
+    "shortcut of auth_init for localhost"
+    auth_init()
+
+@hosts('localhost')
+def local_start_server():
+    "shortcut of start_server for localhost"
+    start_server(llocal)
+
+wget = "wget --keep-session-cookies --load-cookies {cookie_file} "
+tenant = "http://{host_string}:{http_port}/collectionspace/tenant/{tenant}"
+
+def _get_cookie():
+    "login to collectionspace and GET tenant init to reload the configs"
+    env.cookie_file = "cookies.txt"
+    env.login_password = prompt("enter password for {}:".format(env.login_userid))
+    llocal("""\
+          wget -q --keep-session-cookies --save-cookies {cookie_file} \
+          --post-data "userid={login_userid}&password={login_password}" \
+          http://{host_string}:{http_port}/collectionspace/tenant/{tenant}/login \
+          -O /dev/null \
+          """)
+
+    cookie = open(env.cookie_file).readlines().pop()
+    if "CSPACESESSID" in cookie:
+        print(green("logged in successfully"))
+    else:
+        print(red("not logged in! wrong password?"))
+        exit
+
+
+@_login
+def tenant_init():
+    "hit tenant/{tenant}/init to reload configs"
+    llocal(wget + tenant + '/init' + '-O -')
+
+@_login
+def auth_init():
+    "hit authorities/initialise and authorities/vocab/initialize"
+    llocal(wget + tenant + '/authorities/initialise' + ' -O -')
+    llocal(wget + tenant +'/authorities/vocab/initialize' + ' -O - ')#!!z!!
 
 def test_authority(authority):
     "use the ?authorities naming style; so org for orgauthorities and person for personauthorities"
-    env.authorities = authority + 'authorities'
+    if authority == 'organization':
+        env.authorities = 'org' + 'authorities'
+    else:
+        env.authorities = authority + 'authorities'
+    env.authority = authority
     env.login_password = prompt("enter password for {}:".format(env.login_userid))
-    llocal('wget --user={login_userid} --password={login_password} --keep-session-cookies \
-           http://{host_string}:{http_port}/cspace-services/{authorities} -O authorities.xml ')
+    _compose_post_data(authority)
+    authorities_list = llocal('wget --user={login_userid} --password={login_password}\
+                              --keep-session-cookies \
+                              http://{host_string}:{http_port}/cspace-services/{authorities}\
+                              -O - ', True)
+    csid = _find_default_authority_csid(authorities_list)
+    success = _post_item_to_authority(authority,csid)
+    if success:
+        llocal('rm {}.xml'.format(authority))
 
-    string = open('authorities.xml').read()
+def _find_default_authority_csid(string):
     tree = objectify.fromstring(string)
     items = [(item.displayName,item.csid)  for item in tree.findall('list-item')]
     item = filter(lambda item: 'Test' not in str(item[0]), items)
     csid = item[0][1]
-    _compose_post_data(authority)
-    _post_item_to_authority(authority,csid)
-    llocal('rm authorities.xml')
-    llocal('rm {}.xml'.format(authority))
-
+    return csid
 
 def _compose_post_data(authority):
     string = open('authority_template.xml').read()
@@ -161,8 +190,10 @@ def _compose_post_data(authority):
     env.short_display_name = authority[:3]
     env.description = 'test authority'
     env.tenant_schema_name = authority + 's_' + env.tenant
-    env.content = '<employer>Mr. Testy</employer> \
-                   <assistant>Mr. Helper</assistant>'
+    # env.content = '<employer>Mr. Testy</employer> \
+    #                <assistant>Mr. Helper</assistant>'
+    env.content = '<fakeName>Mr. Testy</fakeName>'
+
     doc = string.format(**env)
     new_file.write(doc)
     new_file.close()
@@ -170,15 +201,20 @@ def _compose_post_data(authority):
 
 def _post_item_to_authority(authority,csid):
     "use the name of the xml file in this directory without the .xml"
-    env.authorities = authority + 'authorities'
-    env.authority = authority
     env.csid = csid
     response = llocal("curl -i -u {login_userid}:{login_password} -X POST -H \"Content-Type: application/xml\" \
            http://{host_string}:{http_port}/cspace-services/{authorities}/{csid}/items \
            -T {authority}.xml", True)
     tuples = filter(lambda t: ': ' in t, response.split('\r\n'))
     dd = dict(map(lambda string: tuple(string.split(': ')) , tuples))
-    _view_item(dd['Location'])
+    if 'Location' not in dd:
+        puts(red('Create Request Failed'))
+        puts(response)
+        return False
+    else:
+        _view_item(dd['Location'])
+        return True
+
 
 def _view_item(location):
     env.location = location
